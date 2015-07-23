7841cd0fa464f504bb8662680b9aa56bf5d1cf1d
Brandon Pomeroy <pomeroyb@uw.edu>
2015-07-22 23:16:28 -0700

    Finished exercise 2

---
+++ Exercise 2/ex2.sql
@@ -0,0 +1,32 @@
+CREATE TABLE person (
+	id INTEGER PRIMARY KEY,
+	first_name TEXT,
+	last_name TEXT,
+	age INTEGER
+);
+
+CREATE TABLE pet (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	breed TEXT,
+	age INTEGER,
+	dead INTEGER
+);
+
+CREATE TABLE person_pet (
+	person_id INTEGER,
+	pet_id INTEGER
+);
+
+CREATE TABLE car (
+	id INTEGER PRIMARY KEY,
+	year INTEGER,
+	make TEXT,
+	model TEXT
+);
+
+CREATE TABLE person_car (
+	id INTEGER PRIMARY KEY,
+	person_id INTEGER,
+	car_id INTEGER
+);
